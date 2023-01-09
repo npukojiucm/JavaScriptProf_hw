0857bbf5f4076239eeeedb726cfdfe0c8d5a9661
Alexandr <a.platcev@ya.ru>
2023-01-09 23:09:00 +0300

    correction webpack.config.js

--- Модули/webpack.config.js
+++ Модули/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
-            filename: "main.html"
+            filename: "index.html"
         }),
         new MiniCssExtractPlugin({
             filename:'[name].css'
